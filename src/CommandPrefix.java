9d89da223850a321e289796292022711be5ee750
z <benjaminschoeffthaler@gmail.com>
2021-03-10 20:33:14 +0100

    Commands

---
+++ src/CommandPrefix.java
@@ -0,0 +1,35 @@
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class CommandPrefix extends ListenerAdapter {
+    @Override
+    public void onMessageReceived(@NotNull MessageReceivedEvent event) {
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().startsWith(Main.bot.settings.getPrefix()) && msg.getContentRaw().contains("prefix")) {
+            MessageChannel channel = event.getChannel();
+            StringBuilder message = new StringBuilder();
+            String cmd = null;
+            if (msg.getContentRaw().trim().contains(" ")) {
+                cmd = msg.getContentRaw().trim().split(" ")[1];
+            }
+
+            if (cmd != null && cmd.trim().equals("set")) {
+                Main.bot.settings.setPrefix(msg.getContentRaw().trim().split(" ")[2]);
+                message.append("Prefix set to:\n");
+                message.append(msg.getContentRaw().trim().split(" ")[2]);
+            } else if (cmd == null || cmd.trim().equals("get")) {
+                message.append("Current prefix is:\n");
+                message.append(Main.bot.settings.getPrefix());
+            }
+
+            String finalMessage = message.toString();
+            if (!finalMessage.equals(""))
+                channel.sendMessage(finalMessage).queue();
+            else
+                channel.sendMessage("There was an error processing your request").queue();
+        }
+    }
+}
---
+++ src/CommandWc.java
@@ -0,0 +1,84 @@
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+
+public class CommandWc extends ListenerAdapter {
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().startsWith(Main.bot.settings.getPrefix()) && msg.getContentRaw().contains("wc")) {
+            MessageChannel channel = event.getChannel();
+
+            JSONObject json = null;
+            try {
+                json = Main.bot.readJsonFromUrl("https://api.wynncraft.com/public_api.php?action=onlinePlayers");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            assert json != null;
+
+            String cmd = null;
+            if (msg.getContentRaw().trim().contains(" ")) {
+                cmd = msg.getContentRaw().trim().split(" ")[1];
+            }
+
+            StringBuilder message = new StringBuilder();
+            Iterator<String> keys = json.keys();
+
+            if (Main.bot.isNumeric(cmd)) {
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    if (key.equals("request"))
+                        continue;
+
+                    if (json.get(key) instanceof JSONArray && key.equals("WC" + cmd)) {
+                        message.append("> :earth_americas: ").append(key).append(": ").append(((JSONArray) json.get(key)).length()).append("\r\n");
+                        for (int i = 0; i < ((JSONArray) json.get(key)).length(); i++) {
+                            message.append(((JSONArray) json.get(key)).get(i));
+                            message.append("\n");
+                        }
+                    }
+                }
+            } else if (cmd != null && cmd.trim().equals("chest")) {
+
+            } else {
+                HashMap<String, Integer> wcMap = new HashMap<>();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    if (key.equals("request"))
+                        continue;
+
+                    if (json.get(key) instanceof JSONArray) {
+                        if (((JSONArray) json.get(key)).length() != 0) {
+                            wcMap.put(key, ((JSONArray) json.get(key)).length());
+                        }
+                    }
+                }
+                wcMap = Main.bot.sortByValue(wcMap);
+                int i = 1;
+                for (Map.Entry<String, Integer> en : wcMap.entrySet()) {
+                    message.append(":earth_americas: ").append(en.getKey()).append(": ").append(en.getValue()).append("\r\n");
+                    i++;
+                    if (i > 10)
+                        break;
+                }
+            }
+
+            String finalMessage = message.toString();
+            if (!finalMessage.equals(""))
+                channel.sendMessage(finalMessage).queue();
+            else
+                channel.sendMessage("There was an error processing your request").queue();
+        }
+    }
+}
