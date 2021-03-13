c58d7e99ed32043995c440e158e4e60d82138153
zipfl <benjaminschoeffthaler@gmail.com>
2021-03-13 14:54:28 +0100

    Quest & Level requirements

--- src/ParseThread.java
+++ src/ParseThread.java
@@ -1,4 +1,5 @@
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.BufferedWriter;
@@ -9,90 +10,82 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-@SuppressWarnings("BusyWait")
 public class ParseThread extends Thread {
     private final String mode;
-    private final String filename;
+    private final String inputFile;
 
-    public ParseThread(String filename, String mode) {
-        this.filename = filename;
+    public ParseThread(String inputFile, String mode) {
+        this.inputFile = inputFile;
         this.mode = mode;
     }
 
     @Override
     public void run() {
         try {
-            if (mode.equals("player")) {
-                if (Bot.getLogLineCount("onlinePlayers.log") == 0) {
-                    JSONObject json = new JSONObject(Bot.readLog(filename));
-                    ArrayList<String> onlinePlayers = new ArrayList<>();
-                    Iterator<String> keys = json.keys();
-                    while (keys.hasNext()) {
-                        String key = keys.next();
-                        if (key.equals("request"))
-                            continue;
+            if (mode.equals("online")) {
+                if (Bot.getLogLineCount(inputFile) == 0)
+                    return;
+                JSONObject json = new JSONObject(Bot.readLog(inputFile));
+                ArrayList<String> onlinePlayers = new ArrayList<>();
+                Iterator<String> keys = json.keys();
+                while (keys.hasNext()) {
+                    String key = keys.next();
+                    if (key.equals("request"))
+                        continue;
 
-                        if (json.get(key) instanceof JSONArray) {
-                            for (int i = 0; i < ((JSONArray) json.get(key)).length(); i++) {
-                                onlinePlayers.add(((JSONArray) json.get(key)).getString(i));
-                            }
+                    if (json.get(key) instanceof JSONArray) {
+                        for (int i = 0; i < ((JSONArray) json.get(key)).length(); i++) {
+                            onlinePlayers.add(((JSONArray) json.get(key)).getString(i));
                         }
                     }
+                }
 
-                    FileWriter fw = new FileWriter("onlinePlayers.log");
-                    for (String player : onlinePlayers) {
-                        fw.write(player + "\n");
-                    }
-                    fw.close();
+                FileWriter fw = new FileWriter("onlinePlayers.log");
+                for (String player : onlinePlayers) {
+                    fw.write(player + "\n");
                 }
-            } else if (mode.equals("chests")) {
-                String chestLog = Bot.readLog(filename);
-                while (chestLog.length() == 0) {
-                    Thread.sleep(1000);
-                    chestLog = Bot.readLog(filename);
-                    if (chestLog.length() != 0) {
-                        break;
-                    }
-                }
-                JSONObject json;
-                long timestamp;
-                String[] allPlayerStats = chestLog.split("\n");
+                fw.close();
+
+            } else if (mode.equals("player")) {
+                if (Bot.getLogLineCount(inputFile) == 0)
+                    return;
+                String playerStats = Bot.readLog(inputFile);
+                String[] allPlayerStats = playerStats.split("\n");
                 for (String playerStat : allPlayerStats) {
-                    Bot.removeFirstLine("playerStats.log");
-                    json = new JSONObject(playerStat);
+                    Bot.removeFirstLine(inputFile);
+                    JSONObject json = new JSONObject(playerStat);
                     Iterator<String> keys = json.keys();
                     while (keys.hasNext()) {
                         String key = keys.next();
-                        timestamp = json.getLong("timestamp");
+                        long timestamp = json.getLong("timestamp");
                         if (key.equals("data")) {
-                            if (json.get(key) instanceof JSONArray) {
-                                String username = json.getJSONArray("data").getJSONObject(0).getString("username");
-                                String server = null;
-                                if (json.getJSONArray("data").getJSONObject(0).getJSONObject("meta").getJSONObject("location").getBoolean("online")) {
-                                    server = json.getJSONArray("data").getJSONObject(0).getJSONObject("meta").getJSONObject("location").getString("server");
-                                }
+                            String username = json.getJSONArray("data").getJSONObject(0).getString("username");
+                            String server = null;
+                            if (json.getJSONArray("data").getJSONObject(0).getJSONObject("meta").getJSONObject("location").getBoolean("online")) {
+                                server = json.getJSONArray("data").getJSONObject(0).getJSONObject("meta").getJSONObject("location").getString("server");
+                            }
+                            int chests = json.getJSONArray("data").getJSONObject(0).getJSONObject("global").getInt("chestsFound");
 
-                                int chests = json.getJSONArray("data").getJSONObject(0).getJSONObject("global").getInt("chestsFound");
-                                if (chests < 1500) {
-                                    addToBlacklist(username);
+                            if (getHighestCombatLevel(json) < 100 || !hasQuest(json, "One Thousand Meters Under") || chests < 1500) {
+                                addToBlacklist(username);
+                            } else {
+                                if (server != null && !server.equals("null")) {
+                                    BufferedWriter bf = new BufferedWriter(new FileWriter("chests_" + server + ".log", true));
+                                    bf.write(username + "," + chests + "," + timestamp);
+                                    System.out.println("[CST] " + server + ": " + username + "," + chests + "," + timestamp);
+                                    bf.newLine();
+                                    bf.flush();
+                                    bf.close();
                                 } else {
-                                    if (server != null && !server.equals("null")) {
-                                        BufferedWriter bf = new BufferedWriter(new FileWriter("chests_" + server + ".log", true));
-                                        bf.write(username + "," + chests + "," + timestamp);
-                                        System.out.println("[CST] " + server + ": " + username + "," + chests + "," + timestamp);
-                                        bf.newLine();
-                                        bf.flush();
-                                        bf.close();
-                                    } else {
-                                        System.out.println("[OFF] " + username + " is already offline..");
-                                    }
+                                    System.out.println("[OFF] " + username + " is already offline..");
                                 }
                             }
+
                         }
                     }
                 }
             }
-        } catch (IOException | InterruptedException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
@@ -114,5 +107,47 @@ public class ParseThread extends Thread {
         }
         bf.flush();
         bf.close();
+    }
+
+    public static boolean hasQuest(JSONObject playerJSON, String questName) {
+        Iterator<String> keys = playerJSON.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            if (key.equals("data")) {
+                JSONArray classes = playerJSON.getJSONArray("data").getJSONObject(0).getJSONArray("classes");
+                for (int i = 0; i < classes.length(); i++) {
+                    JSONArray completedQuests = classes.getJSONObject(i).getJSONObject("dungeons").getJSONArray("list");
+                    for (int j = 0; j < completedQuests.length(); j++) {
+                        if (completedQuests.getJSONObject(j).getString("name").equals(questName))
+                            return true;
+                    }
+                }
+
+            }
+        }
+        return false;
+    }
+
+    public static int getHighestCombatLevel(JSONObject playerJSON) {
+        Iterator<String> keys = playerJSON.keys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            if (key.equals("data")) {
+                JSONArray classes = playerJSON.getJSONArray("data").getJSONObject(0).getJSONArray("classes");
+                int highestLevel = 0;
+                for (int i = 0; i < classes.length(); i++) {
+                    try {
+                        int level = classes.getJSONObject(i).getJSONObject("professions").getJSONObject("combat").getInt("level");
+                        if (level > highestLevel) {
+                            highestLevel = level;
+                        }
+                    } catch (JSONException ignored) {
+                    }
+
+                }
+                return highestLevel;
+            }
+        }
+        return -1;
     }
 }
