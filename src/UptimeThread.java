622d99f611a61ae03e9166aefa9ddb0c13124c9f
zipfl <benjaminschoeffthaler@gmail.com>
2021-03-13 02:45:40 +0100

    Fixed bug with 1 digit WCs

--- src/UptimeThread.java
+++ src/UptimeThread.java
@@ -1,6 +1,7 @@
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -8,7 +9,7 @@ public class UptimeThread extends Thread {
     private final HashMap<String, ServerStatus> uptimeMap = new HashMap<>();
 
     public UptimeThread() {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 80; i++) {
             ServerStatus serverStatus = new ServerStatus(false, 0L);
             uptimeMap.put("WC" + i, serverStatus);
         }
@@ -18,14 +19,19 @@ public class UptimeThread extends Thread {
     public void run() {
         try {
             getUptimeMap(uptimeMap);
-            String wcLog = Bot.readLog("wc.log").toString();
-            for (int i = 1; i < 80; i++) {
-                if (wcLog.contains("WC" + i)) {
-                    if (!uptimeMap.get("WC" + i).isOnline) {
-                        uptimeMap.put("WC" + i, new ServerStatus(true, System.currentTimeMillis()));
+            String apiLog = Bot.readLog("wc.log").toString();
+            String[] uptimeLog = Bot.readLog("uptime.log").toString().split("\n");
+            ArrayList<String> allServers = new ArrayList<>();
+            for (String uptimeLogEntry : uptimeLog) {
+                allServers.add(uptimeLogEntry.split(",")[0]);
+                for (String server : allServers) {
+                    if (apiLog.contains(server)) {
+                        if (!server.equals("") && !uptimeMap.get(server).isOnline) {
+                            uptimeMap.put(server, new ServerStatus(true, System.currentTimeMillis()));
+                        }
+                    } else {
+                        uptimeMap.put(server, new ServerStatus(false, System.currentTimeMillis()));
                     }
-                } else {
-                    uptimeMap.put("WC" + i, new ServerStatus(false, System.currentTimeMillis()));
                 }
             }
             BufferedWriter bf = new BufferedWriter(new FileWriter("uptime.log"));
