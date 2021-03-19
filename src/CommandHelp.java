438fd9b359e4275b8cf9a49d974bc07f7626f54b
zipfl <benjaminschoeffthaler@gmail.com>
2021-03-19 11:05:31 +0100

    Updated help text

--- src/CommandHelp.java
+++ src/CommandHelp.java
@@ -8,12 +8,15 @@ public class CommandHelp extends ListenerAdapter {
         String msg = event.getMessage().getContentRaw();
         if (msg.startsWith(Main.bot.settings.getPrefix(event.getGuild().toString())) && msg.indexOf("help") == Main.bot.settings.getPrefix(event.getGuild().toString()).length()) {
             String message = "🤔\n" +
-                    "c - Shows the best servers to loot\n" +
-                    "c <n> - Shows loot history of the server\n" +
+                    "c - Show chests looted in the last 2h per server\n" +
+                    "c <n> - Shows loot history of server WC<n>\n" +
                     "fu <n> - Force update server <n>\n" +
-                    "up <n> - Show server\n" +
-                    "wc <n> - Show  <n>\n" +
-                    "prefix set <> - Change bot prefix\n";
+                    "fu <p> - Force update player <p>\n" +
+                    "up - Show server uptimes\n" +
+                    "up <n> - Show uptime of WC<n>\n" +
+                    "wc - Show player count per server\n" +
+                    "wc <n> - Show player list of WC<n>\n" +
+                    "prefix set <prefix> - Change bot prefix\n";
             Bot.sendMessage(event.getChannel(), message);
         }
     }
