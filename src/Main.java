1fb98651f38ab00074ca1b38d0b8c5e215822eeb
zipfl <benjaminschoeffthaler@gmail.com>
2022-11-24 20:48:11 +0100

    Removed wynnCraft API (wynn API discontinued), JDA 5.0, SlashCommands

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,26 @@
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
 
-import javax.security.auth.login.LoginException;
-import java.io.IOException;
+import java.util.Collections;
 
 public class Main extends ListenerAdapter {
-    public static Bot bot;
-
-    public static void main(String[] args) throws LoginException, IOException {
+    public static void main(String[] args) {
         if (args.length < 1) {
             System.out.println("You have to provide a token as first argument!");
             System.exit(1);
         }
-        bot = new Bot(args[0]);
+
+        JDA jda = JDABuilder.createLight(args[0], Collections.emptyList())
+                .addEventListeners(new SlashCommands())
+                .setActivity(Activity.playing("ligo"))
+                .build();
+
+        jda.upsertCommand("say", "less").queue();
+        jda.upsertCommand("ping", "Calculate ping of the bot").queue();
+        jda.upsertCommand("sussy", "Say a sussy quote").queue();
+        jda.upsertCommand("sus", "Amogus").addOption(OptionType.STRING, "sus", "Imposter").queue();
     }
 }
--- src/ServerStatus.java
+++
@@ -1,9 +0,0 @@
-class ServerStatus {
-    public boolean isOnline;
-    public long changed;
-
-    public ServerStatus(boolean isOnline, long changed) {
-        this.isOnline = isOnline;
-        this.changed = changed;
-    }
-}
